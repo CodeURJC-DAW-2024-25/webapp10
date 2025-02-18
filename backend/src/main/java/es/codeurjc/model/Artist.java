58a577ba9f267fb111091ed7836a8dfacc8a0339
nataliaM4 <n.mendez.2022@alumnos.urjc.es>
2025-02-18 10:40:46 +0100

    artist and concert entities

--- backend/src/main/java/es/codeurjc/model/Artist.java
+++ backend/src/main/java/es/codeurjc/model/Artist.java
@@ -1,5 +1,43 @@
 package es.codeurjc.model;
 
 public class Artist {
+    
+    private String artistName;
+    private String musicalStyle;
+    private String artistInfo;
 
+    //Constructors
+    public Artist(){}
+
+    public Artist (String artistName, String musicalStyle, String artisInfo) {
+        this.artistName = artistName;
+        this.musicalStyle = musicalStyle;
+        this.artistInfo = artisInfo;
+    }
+
+    //Getters and setters
+    
+    public void setArtistName (String artistName){
+        this.artistName = artistName;
+    }
+
+    public String getArtistName(){
+            return artistName;
+        }
+
+    public void setMusicalStyle (String musicalStyle){
+        this.musicalStyle = musicalStyle;
+    }
+
+    public String getMuscialStyle(){
+            return musicalStyle;
+        }
+
+    public void setArtistInfo (String artistName){
+        this.artistName = artistName;
+    }
+
+    public String getArtistInfo(){
+        return artistInfo;
+    }
 }
--- backend/src/main/java/es/codeurjc/model/Concert.java
+++ backend/src/main/java/es/codeurjc/model/Concert.java
@@ -1,5 +1,86 @@
 package es.codeurjc.model;
 
-public class Concert {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+
+public class Concert {
+    private String concertName; 
+    private String artistName;
+    private String artistInfo;
+    private String concertDetails;
+    private LocalDate concertDate;
+    private LocalTime concertTime;
+    private String location;
+
+    //Constructors
+    public Concert(){}
+    
+    public Concert(String concertName, String artistName, String artistInfo, String concertDetails, LocalDate concertDate, LocalTime concertTime, String location){
+        this.concertName = concertName;
+        this.artistName = artistName;
+        this.artistInfo = artistInfo;
+        this.concertDetails = concertDetails;
+        this.concertDate = concertDate;
+        this.concertTime = concertTime;
+        this.location = location;
+    }
+
+    //Getters and setters
+
+    public String getConcertName() {
+        return concertName;
+    }
+
+    public void setConcertName(String concertName) {
+        this.concertName = concertName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistInfo() {
+        return artistInfo;
+    }
+
+    public void setArtistInfo(String artistInfo) {
+        this.artistInfo = artistInfo;
+    }
+
+    public String getConcertDetails() {
+        return concertDetails;
+    }
+
+    public void setConcertDetails(String concertDetails) {
+        this.concertDetails = concertDetails;
+    }
+
+    public LocalDate getConcertDate() {
+        return concertDate;
+    }
+
+    public void setConcertDate(LocalDate concertDate) {
+        this.concertDate = concertDate;
+    }
+
+    public LocalTime getConcertTime() {
+        return concertTime;
+    }
+
+    public void setConcertTime(LocalTime concertTime) {
+        this.concertTime = concertTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }

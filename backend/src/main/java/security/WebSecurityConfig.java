96afa4efe0a6fa198ca6e5d3fd63918073df0da2
Irene García López <117302264+irenegarccia@users.noreply.github.com>
2025-02-24 12:08:50 +0100

    Security Changes

---
+++ backend/src/main/java/security/WebSecurityConfig.java
@@ -0,0 +1,78 @@
+package es.codeurjc.daw.library.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Autowired
+	RepositoryUserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http.authorizeHttpRequests(authorize -> authorize
+						// PUBLIC PAGES
+						.requestMatchers("/").permitAll()
+						.requestMatchers("/images/**").permitAll() 
+						.requestMatchers("/register/**").permitAll()
+                        .requestMatchers("/concertInfo/**").permitAll()
+						// PRIVATE PAGES
+						.requestMatchers("/purchasePage").hasAnyRole("USER")
+						.requestMatchers("/userPage").hasAnyRole("USER")
+                        .requestMatchers("/newArtist/*").hasAnyRole("ADMIN")
+						.requestMatchers("/newCocert/*").hasAnyRole("ADMIN"))
+				.formLogin(formLogin -> formLogin
+						.loginPage("/login")
+						.failureUrl("/loginerror")
+						.defaultSuccessUrl("/")
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/")
+						.permitAll());
+
+		return http.build();
+	}
+}

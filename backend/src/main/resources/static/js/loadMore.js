417ece3284bf41c56dfc3656a915422ed42fb142
Garrobo08 <andreagarroboguzman@gmail.com>
2025-02-28 15:25:08 +0100

    Implements spinner

--- backend/src/main/resources/static/js/loadMore.js
+++ backend/src/main/resources/static/js/loadMore.js
@@ -3,13 +3,24 @@ let page = 0;
 async function loadMore() {
     page++;
 
-    const response = await fetch(`/moreConcerts?page=${page}`);
-    const data = await response.text();
+    document.getElementById("spinner").style.display = "block";
 
-    document.getElementById("allConcerts").innerHTML += data;
-
-    if (data.includes("<!--true-->")) {
-        document.getElementById('load-more-btn').style.display = 'none';
+    try{
+        const response = await fetch(`/moreConcerts?page=${page}`);
+        const data = await response.text();
+    
+    
+    
+        document.getElementById("allConcerts").innerHTML += data;
+    
+        if (data.includes("<!--true-->")) {
+            document.getElementById('load-more-btn').style.display = 'none';
+        }
+    } finally{
+        document.getElementById("spinner").style.display = "none";
     }
+
+
+    
 
 }

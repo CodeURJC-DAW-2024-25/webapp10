eab13bc05bd004ff75c9b767c641385557e7398d
Irene <i.garcialop.2021@alumnos.urjc.es>
2025-03-03 11:31:11 +0100

    Add concert name to index and change bar graphics color

--- backend/src/main/resources/static/js/barGraphics.js
+++ backend/src/main/resources/static/js/barGraphics.js
@@ -3,36 +3,50 @@ fetch("/ticketsByConcert")
 .then(data => {
     const ctx = document.getElementById("graphicBar").getContext("2d");
 
-    // Generar un color aleatorio para cada concierto
     function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 
-    const colors = data.labels.map(() => getRandomColor()); // Crear un color aleatorio para cada concierto
+    const colors = data.labels.map(() => getRandomColor()); 
 
     new Chart(ctx, {
-    type: "bar",
-    data: {
-        labels: data.labels,
-        datasets: [{
-        label: "Entradas Vendidas",
-        data: data.data,
-        backgroundColor: colors, // Asignar colores aleatorios a las barras
-        }]
-    },
-    options: {
-        responsive: true,
-        scales: {
-        y: { 
-            beginAtZero: true 
+        type: "bar",
+        data: {
+            labels: data.labels,
+            datasets: [{
+                label: "Tickets Sold",
+                data: data.data,
+                backgroundColor: colors,
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white'
+                    }
+                },
+                y: { 
+                    beginAtZero: true,
+                    ticks: {
+                        color: 'white'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        color: 'white' 
+                    }
+                }
+            }
         }
-        }
-    }
     });
 })
-.catch(error => console.error("Error al obtener los datos del gráfico:", error));
+.catch(error => console.error("Error fetching chart data:", error));

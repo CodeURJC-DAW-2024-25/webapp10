545dc5ec254f794c51c81df99cd7ad6625b0142c
Garrobo08 <andreagarroboguzman@gmail.com>
2025-03-17 14:34:05 +0100

    Security in Spring

--- backend/src/main/java/es/codeurjc/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/es/codeurjc/backend/security/WebSecurityConfig.java
@@ -3,19 +3,34 @@ package es.codeurjc.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import es.codeurjc.backend.security.jwt.UnauthorizedHandlerJwt;
+import es.codeurjc.backend.security.jwt.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
 	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
 	RepositoryUserDetailsService userDetailsService;
+
+	@Autowired
+	private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
 
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
@@ -27,13 +42,67 @@ public class WebSecurityConfig {
 		return authProvider;
 	}
 
+	// Verify credentials and determine if a user is valid.
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http
+				.securityMatcher("/api/**")
+				.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+		http.authorizeHttpRequests(authorize -> authorize
+				// cambiar esto de las redirecciones y las rutas OJOOOOOOO!!!!
+				// PRIVATE PAGES
+				.requestMatchers(HttpMethod.GET, "/api/editconcert/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.PUT, "/api/editArtist/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/deleteArtist/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/concert/delete/**").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/concert/purchasePage/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/user/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/userPage").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.PUT, "/api/edituser/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.POST, "/api/newartist").hasAnyRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/api/newconcert").hasAnyRole("ADMIN")
+
+				// PUBLIC PAGES
+				.anyRequest().permitAll()
+
+		);
+
+		// Disable Form login Authentication
+		http.formLogin(formLogin -> formLogin.disable());
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf(csrf -> csrf.disable());
+
+		// Disable Basic Authentication
+		http.httpBasic(httpBasic -> httpBasic.disable());
+
+		// Stateless session
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
 
 		http.authenticationProvider(authenticationProvider());
 

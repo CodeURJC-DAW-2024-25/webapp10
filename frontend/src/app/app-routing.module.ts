04136f1ffe41426186ea83f0d2164763bf9d1add
nataliaM4 <n.mendez.2022@alumnos.urjc.es>
2025-04-24 21:21:19 +0200

    Add new artist and edit artist functionality

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,11 +8,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserPageComponent } from './components/user/user-page.component';
 import { ConcertFormComponent } from './components/concerts/concert-form.component';
+import { ArtistFormComponent } from './components/artists/artist-form.component';
 
 const appRoutes: Routes = [
   { path: '', component: ConcertsComponent },
   { path: 'new-concert', component: ConcertFormComponent },
   { path: 'edit-concert/:id', component: ConcertFormComponent },
+  { path: 'edit-artist/:id', component: ArtistFormComponent },
+  { path: 'new-artist', component: ArtistFormComponent },
   { path: 'edit-user/:id', component: EditUserComponent},
   { path: 'user-page/:id', component: UserPageComponent},
   { path: 'error/:type', component: ErrorComponent },

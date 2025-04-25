8a785b62bb02a6893c7e9bd98c69c96152ee9a2d
Garrobo08 <andreagarroboguzman@gmail.com>
2025-04-25 13:49:52 +0200

    Load more functionality

--- frontend/src/app/components/concerts/concerts.component.ts
+++ frontend/src/app/components/concerts/concerts.component.ts
@@ -17,6 +17,7 @@ export class ConcertsComponent implements OnInit {
   loading: boolean = false;
   successMessage: string | null = null;
   errorMessage: string | null = null;
+  hasMore: boolean = true;
 
   constructor(
     private http: HttpClient,
@@ -25,9 +26,32 @@ export class ConcertsComponent implements OnInit {
   ) {}
 
   public ngOnInit() {
-    this.concertService.getConcerts().subscribe(
-      (concerts) => (this.concerts = concerts),
-      (error) => console.log(error)
+    this.loadConcerts();
+  }
+
+  loadConcerts() {
+    this.loading = true;
+    this.concertService.getConcerts(this.page, this.size).subscribe(
+      (concerts) => {
+        if (concerts.length > 0) {
+          this.concerts = this.concerts.concat(concerts); // Añadir los nuevos conciertos
+          this.page++; // Incrementar la página para la siguiente carga
+        } else {
+          this.hasMore = false; // No hay más conciertos para cargar
+        }
+        this.loading = false;
+      },
+      (error) => {
+        console.log(error);
+        this.errorMessage = "Error al cargar conciertos.";
+        this.loading = false;
+      }
     );
   }
+
+  loadMore() {
+    if (this.hasMore) {
+      this.loadConcerts(); // Cargar más conciertos
+    }
+}
 }
